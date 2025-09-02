a528d0a430d2aec706b62d57424c484de12512c6
Ariane Hoyos <n00477677@upn.pe>
2025-09-02 18:10:56 -0500

    Agregar archivos de proyecto.

---
+++ TallerC#/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TallerC_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Escribir mensaje 
+            Console.WriteLine("Bienvenido a C#");
+
+            //--Definir variables--
+            int a, b, producto;
+            
+            //--Entrada datos--
+            Console.WriteLine("Ingrese primer numero");
+            //--Guardar un valor en la consola--
+            a = Convert.ToInt32(Console.ReadLine());
+            //a= int. TryParse(Console.ReadLine()); //  Boolean True or False
+
+            //--Entrada datos--
+            Console.WriteLine("Ingrese segundo numero");
+            //--Guardar un valor en la consola--
+            b = Convert.ToInt32(Console.ReadLine());
+            //b= int. TryParse(Console.ReadLine()); //  Boolean True or False
+
+            //-- Proceso --
+            producto = a * b;
+
+            //-- Salida datos --
+            Console.WriteLine("El producto es:" + producto);
+            Console.WriteLine($"El producto de {a} * {b} = {producto}");
+
+
+
+            //Detener la pantalla para mostrar el resultado
+            Console.ReadKey();
+        }
+    }
+}
